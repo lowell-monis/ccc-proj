41144fcd6ed1de080fa8404d9ea0a168a6287854
lowell-monis <monislow@msu.edu>
2025-03-19 13:11:38 -0400

    created data object and modified repo structure

--- figure.R
+++ figure.R
@@ -1,1 +1,12 @@
-a
+library(tidyverse)
+
+ccc1 <- read.csv("data/ccc_compiled_20172020.csv")
+ccc1 <- ccc1 %>% 
+  select(-starts_with("source"))
+
+ccc2 <- read.csv("data/ccc_compiled_20212024.csv")
+ccc2 <- ccc2 %>% 
+  select(-starts_with("source"))
+ccc <- bind_rows(ccc1, ccc2)
+
+ccc
